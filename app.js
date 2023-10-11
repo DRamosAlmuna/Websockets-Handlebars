40b75ec22ef439fa12c28da1312b36cc5f9e1867
Deivi Ramos <deivi.ramos@usm.cl>
2023-10-10 23:59:57 -0300

    first commit

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import exphbs from 'express-handlebars';
+import path from 'path';
+
 import productsRouter from './src/routes/products.js';
 import cartsRouter from './src/routes/carts.js';
 
 const app = express();
-const PORT = 8080;
+const server = http.createServer(app);
+const io = new Server(server);
 
+// Configurar Handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Configurar rutas
 app.use(express.json());
-
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-app.listen(PORT, () => {
+// Ruta para la vista "realTimeProducts.handlebars"
+app.get('/realTimeProducts', (req, res) => {
+  res.render('realTimeProducts');
+});
+
+// Configurar Socket.io para websockets
+io.on('connection', (socket) => {
+  console.log('Un cliente se ha conectado.');
+
+  socket.on('producto-creado', (nuevoProducto) => {
+    io.emit('producto-creado', nuevoProducto);
+  });
+
+  socket.on('producto-eliminado', (productoEliminado) => {
+    io.emit('producto-eliminado', productoEliminado);
+  });
+});
+
+const PORT = 8080;
+
+server.listen(PORT, () => {
   console.log(`Servidor activo en el puerto ${PORT}`);
 });
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Lo sentimos!, algo ha salido mal');
-  });
+  console.error(err.stack);
+  res.status(500).send('Lo sentimos, algo ha salido mal');
+});
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,83 +1,94 @@
 import express from 'express';
 import fs from 'fs';
+import { Server } from 'socket.io';
 
 const router = express.Router();
 const PRODUCTS_FILE = 'productos.json';
 
+// Socket.io
+let io;
+
+// Configurar Socket.io en la ruta /api/products
+router.setIO = (socketIO) => {
+  io = socketIO;
+};
+
+// Emitir un evento para crear un producto en tiempo real
+function emitProductCreated(product) {
+  if (io) {
+    io.emit('producto-creado', product);
+  }
+}
+
+// Emitir un evento para eliminar un producto en tiempo real
+function emitProductDeleted(productId) {
+  if (io) {
+    io.emit('producto-eliminado', productId);
+  }
+}
+
 // Obtiene los productos del json
 router.get('/', (req, res) => {
+  const limit = req.query.limit;
   const products = JSON.parse(fs.readFileSync(PRODUCTS_FILE, 'utf-8'));
-  const limit = parseInt(req.query.limit);
 
-  if (limit && limit > 0) {
+  if (limit) {
     const limitedProducts = products.slice(0, limit);
     res.json(limitedProducts);
   } else {
     res.json(products);
   }
 });
-  
-  // Obtiene un producto por su ID
-  router.get('/:pid', (req, res) => {
-    const productId = parseInt(req.params.pid);
-    const products = JSON.parse(fs.readFileSync(PRODUCTS_FILE, 'utf-8'));
-    const product = products.find((p) => p.id === parseInt(productId));
-  
-    if (!product) {
-      res.status(404).json({ message: 'Producto no encontrado' });
-    } else {
-      res.json(product);
-    }
-  });
-  
-  // Agrega un nuevo producto
-  router.post('/', (req, res) => {
-    const newProduct = req.body;
-    const products = JSON.parse(fs.readFileSync(PRODUCTS_FILE, 'utf-8'));
-  
-    // Genera ID
-    const lastProductId = products.length > 0 ? products[products.length - 1].id : 0;
-    newProduct.id = lastProductId + 1;
-  
-    products.push(newProduct);
+
+// Obtiene un producto por su ID
+router.get('/:pid', (req, res) => {
+  const productId = parseInt(req.params.pid);
+  const products = JSON.parse(fs.readFileSync(PRODUCTS_FILE, 'utf-8'));
+  const product = products.find((p) => p.id === productId);
+
+  if (!product) {
+    res.status(404).json({ message: 'Producto no encontrado' });
+  } else {
+    res.json(product);
+  }
+});
+
+// Agrega un nuevo producto
+router.post('/', (req, res) => {
+  const newProduct = req.body;
+  const products = JSON.parse(fs.readFileSync(PRODUCTS_FILE, 'utf-8'));
+
+  // Genera ID
+  const lastProductId = products.length > 0 ? products[products.length - 1].id : 0;
+  newProduct.id = lastProductId + 1;
+
+  products.push(newProduct);
+  fs.writeFileSync(PRODUCTS_FILE, JSON.stringify(products, null, 2));
+
+  // Emitir evento de producto creado en tiempo real
+  emitProductCreated(newProduct);
+
+  res.status(201).json(newProduct);
+});
+
+// Elimina un producto por su ID
+router.delete('/:pid', (req, res) => {
+  const productId = parseInt(req.params.pid);
+  const products = JSON.parse(fs.readFileSync(PRODUCTS_FILE, 'utf-8'));
+
+  const index = products.findIndex((p) => p.id === productId);
+
+  if (index === -1) {
+    res.status(404).json({ message: 'Producto no encontrado' });
+  } else {
+    const deletedProduct = products.splice(index, 1)[0];
     fs.writeFileSync(PRODUCTS_FILE, JSON.stringify(products, null, 2));
-  
-    res.status(201).json(newProduct);
-  });
-  
-  // Actualiza un producto por su ID
-  router.put('/:pid', (req, res) => {
-    const productId = parseInt(req.params.pid);
-    const updatedProduct = req.body;
-    const products = JSON.parse(fs.readFileSync(PRODUCTS_FILE, 'utf-8'));
-  
-    const index = products.findIndex((p) => p.id === productId);
-  
-    if (index === -1) {
-      res.status(404).json({ message: 'Producto no encontrado' });
-    } else {
-      products[index] = { ...products[index], ...updatedProduct };
-      fs.writeFileSync(PRODUCTS_FILE, JSON.stringify(products, null, 2));
-  
-      res.json(products[index]);
-    }
-  });
-  
-  // Elimina un producto por su ID
-  router.delete('/:pid', (req, res) => {
-    const productId = parseInt(req.params.pid);
-    const products = JSON.parse(fs.readFileSync(PRODUCTS_FILE, 'utf-8'));
-  
-    const index = products.findIndex((p) => p.id === productId);
-  
-    if (index === -1) {
-      res.status(404).json({ message: 'Producto no encontrado' });
-    } else {
-      products.splice(index, 1);
-      fs.writeFileSync(PRODUCTS_FILE, JSON.stringify(products, null, 2));
-  
-      res.json({ message: 'Producto eliminado' });
-    }
-  });
-  
+
+    // Emitir evento de producto eliminado en tiempo real
+    emitProductDeleted(productId);
+
+    res.json({ message: 'Producto eliminado', deletedProduct });
+  }
+});
+
 export default router;
